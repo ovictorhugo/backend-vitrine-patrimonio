0ef6a48bdcede4880cc86cd57ef362c0da1c8555
gmcosta <geu_costa@outlook.com>
2025-10-19 17:53:52 -0300

    endpoint para estatisticas

--- vitrine/routers/statistics/catalog_statistics.py
+++ vitrine/routers/statistics/catalog_statistics.py
@@ -1,13 +1,21 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from sqlalchemy import and_, func, select
+from sqlalchemy import and_, case, func, literal_column, select
+from sqlalchemy.sql import union_all
 
 from vitrine.dependencies import Session
 from vitrine.models import (
+    Catalog,
     CatalogWorkFlow,
+    CollectionItem,
 )
 
 router = APIRouter(prefix='/statistics', tags=['estatisticas - patrimonio'])
+
+
+class CollectionStatusCount(BaseModel):
+    status: str
+    count: int
 
 
 class CatalogWorkflowCount(BaseModel):
@@ -47,6 +55,38 @@ async def get_catalog_count_by_workflow_status(session: Session):
     )
 
     result = await session.execute(query)
+
+    counts = [
+        {'status': status, 'count': count} for status, count in result.all()
+    ]
+    return counts
+
+
+@router.get(
+    '/catalog/count-by-collection-status',
+    response_model=list[CollectionStatusCount],
+)
+async def get_catalog_count_by_collection_status(session: Session):
+    query_true_false = select(
+        case(
+            (CollectionItem.status == True, 'TRUE'),
+            (CollectionItem.status == False, 'FALSE'),
+        ).label('status'),
+        func.count(CollectionItem.id).label('count'),
+    ).group_by(CollectionItem.status)
+
+    query_not_in_collection = (
+        select(
+            literal_column("'NOT_IN_COLLECTION'").label('status'),
+            func.count(Catalog.id).label('count'),
+        )
+        .outerjoin(CollectionItem, Catalog.id == CollectionItem.catalog_id)
+        .where(CollectionItem.id == None)
+    )
+
+    final_query = union_all(query_true_false, query_not_in_collection)
+
+    result = await session.execute(final_query)
 
     counts = [
         {'status': status, 'count': count} for status, count in result.all()
