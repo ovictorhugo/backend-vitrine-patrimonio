4ad613650c09830821d9dd5a9ee27db8999dc9cb
gmcosta <geu_costa@outlook.com>
2025-10-19 19:28:12 -0300

    filtro em estatistica

--- vitrine/routers/statistics/catalog_statistics.py
+++ vitrine/routers/statistics/catalog_statistics.py
@@ -66,14 +66,43 @@ async def get_catalog_count_by_workflow_status(session: Session):
     '/catalog/count-by-collection-status',
     response_model=list[CollectionStatusCount],
 )
-async def get_catalog_count_by_collection_status(session: Session):
-    query_true_false = select(
-        case(
-            (CollectionItem.status.is_(True), literal_column("'TRUE'")),
-            (CollectionItem.status.is_(False), literal_column("'FALSE'")),
-        ).label('status'),
-        func.count(CollectionItem.id).label('count'),
-    ).group_by(CollectionItem.status)
+async def get_catalog_count_by_collection_status(
+    session: Session,
+    workflow_status: str,
+):
+    latest_workflow_subquery = (
+        select(
+            CatalogWorkFlow.catalog_id,
+            func.max(CatalogWorkFlow.created_at).label('max_created_at'),
+        )
+        .group_by(CatalogWorkFlow.catalog_id)
+        .subquery()
+    )
+
+    query_true_false = (
+        select(
+            case(
+                (CollectionItem.status.is_(True), literal_column("'TRUE'")),
+                (CollectionItem.status.is_(False), literal_column("'FALSE'")),
+            ).label('status'),
+            func.count(CollectionItem.id).label('count'),
+        )
+        .join(Catalog, CollectionItem.catalog_id == Catalog.id)
+        .join(
+            latest_workflow_subquery,
+            Catalog.id == latest_workflow_subquery.c.catalog_id,
+        )
+        .join(
+            CatalogWorkFlow,
+            (Catalog.id == CatalogWorkFlow.catalog_id)
+            & (
+                CatalogWorkFlow.created_at
+                == latest_workflow_subquery.c.max_created_at
+            ),
+        )
+        .where(CatalogWorkFlow.workflow_status == workflow_status)
+        .group_by(CollectionItem.status)
+    )
 
     query_not_in_collection = (
         select(
@@ -81,7 +110,22 @@ async def get_catalog_count_by_collection_status(session: Session):
             func.count(Catalog.id).label('count'),
         )
         .outerjoin(CollectionItem, Catalog.id == CollectionItem.catalog_id)
-        .where(CollectionItem.id.is_(None))
+        .join(
+            latest_workflow_subquery,
+            Catalog.id == latest_workflow_subquery.c.catalog_id,
+        )
+        .join(
+            CatalogWorkFlow,
+            (Catalog.id == CatalogWorkFlow.catalog_id)
+            & (
+                CatalogWorkFlow.created_at
+                == latest_workflow_subquery.c.max_created_at
+            ),
+        )
+        .where(
+            CollectionItem.id.is_(None),
+            CatalogWorkFlow.workflow_status == workflow_status,
+        )
     )
 
     final_query = union_all(query_true_false, query_not_in_collection)
